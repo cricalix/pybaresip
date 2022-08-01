314fde5fafd1c03d18c02467a61ef0b25a3e0e90
Duncan <github.commits@cricalix.net>
2022-08-01 20:45:38 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pybaresip",
+    version="0.0.1",
+    packages=["pybaresipy"],
+    url="https://github.com/cricalix/pybaresip",
+    install_requires=[],
+    license="MIT",
+    author="cricalix",
+    author_email="pybaresip@cricalix.net",
+    description="A Python3 wrapper for baresip",
+)
